e376ec979f24464f97e463c9439d3e8ce88f3b49
OsamaBACS <osama.solution@gmail.com>
2021-07-17 22:41:57 +0300

    ReadFromFirebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,14 +8,22 @@ import { AngularFireDatabase } from 'angularfire2/database';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  courses!: any[];
+  courses$: any;
+  // courses!: any[];
+  // subscription: Subscription;
 
   constructor(db: AngularFireDatabase) {
-    db.list('courses')
-      .valueChanges()
-      .subscribe((courses) => {
-        this.courses = courses;
-        console.log(this.courses);
-      });
+    this.courses$ = db.list('/courses');
+
+    // this.subscription = db
+    //   .list('courses')
+    //   .valueChanges()
+    //   .subscribe((courses) => {
+    //     this.courses = courses;
+    //     console.log(this.courses);
+    //   });
   }
+  // ngOnDestroy(): void {
+  //   this.subscription.unsubscribe();
+  // }
 }
